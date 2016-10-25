62dc29fc0d038caab8d49899ebf794ad72d96198
chetansankhavara <chetan.sankhavara@gmail.com>
2016-10-25 09:45:59 +0530

    initial committ

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define("SITE_URL", "http://api.appsextent.com/");
+define("HOST", "localhost");
+define("DBUSER", "coderede_flame");
+define("DBPASS", "flame@123");
+define("DBNAME", "coderede_flame");
