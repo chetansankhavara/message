9820386919b76ff3a41d8af0cfd57c845886d230
chetansankhavara <chetan.sankhavara@gmail.com>
2016-10-25 09:54:24 +0530

    config database

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 define("SITE_URL", "http://api.appsextent.com/");
 define("HOST", "localhost");
-define("DBUSER", "coderede_flame");
-define("DBPASS", "flame@123");
-define("DBNAME", "coderede_flame");
+define("DBUSER", "root");
+define("DBPASS", "manoj@123");
+define("DBNAME", "message");
